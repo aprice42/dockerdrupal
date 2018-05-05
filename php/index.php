f611f9969f8abfc81020c029712b8b4cb7e0545f
Andy Price <andy@de516n.com>
2018-05-04 20:51:08 -0700

    Initial commit

---
+++ php/index.php
@@ -0,0 +1,24 @@
+<?php
+//$servername = "127.0.0.1";
+//$username = "root";
+//$password = "root1";
+//
+//// Create connection
+//$conn = new mysqli($servername, $username, $password);
+//
+//if ($con) {
+//    $test = '';
+//}
+?>
+
+
+<html>
+<style>
+
+</style>
+<?php 
+  print "Hloow";
+//  print_r($conn);
+?>
+
+</html>
