b51f9bc0db538c5038eac5d8ba87d123b89065c0
Andy Price <andy@de516n.com>
2018-05-11 10:15:56 -0700

    Adds xdebug support

--- php/index.php
+++ php/index.php
@@ -14,11 +14,31 @@
 
 <html>
 <style>
-
+    .luella {
+        font-size: 72px;
+    }
 </style>
-<?php 
-  print "Hloow";
-//  print_r($conn);
+
+<?php
+    $text = "luella is nice\n";
+    $text .= "";
+    print phpinfo();
 ?>
 
+<body>
+    <div class="luella">
+        <?php print $text; ?>
+    </div>
+    <div id="counting">
+        <h2>Counting to 1 million</h2>
+    </div>
+    <script>
+      (function() {
+        var text = document.getElementById("counting");
+        for (i = 0; i < 1000000; i++) {
+          text.innerHTML = "<p>Counting .. " + i + "</p>";
+        }
+      });
+    </script>
+</body>
 </html>
